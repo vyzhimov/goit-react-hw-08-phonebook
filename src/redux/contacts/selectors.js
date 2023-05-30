0e9bb38ef8642a028fedfe00311aa2c5730b9288
Alexander Vyzhymov <vizimov@gmail.com>
2023-05-31 00:41:13 +0300

    fixed-selectors-file

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,17 +1,17 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContactsList = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectContactsFilter = state => state.filter;
 
-export const selectFilteredContacts = state => {
-  const contactItems = selectContactsList(state);
-  const contactFilter = selectContactsFilter(state);
-
-  const filteredContacts = contactItems.filter(
-    ({ name, number }) =>
-      name.toLowerCase().includes(contactFilter.toLowerCase()) ||
-      number.includes(contactFilter)
-  );
-
-  return filteredContacts;
-};
+export const selectFilteredContacts = createSelector(
+  [selectContactsList, selectContactsFilter],
+  (contactItems, contactFilter) => {
+    return contactItems.filter(
+      ({ name, number }) =>
+        name.toLowerCase().includes(contactFilter.toLowerCase()) ||
+        number.includes(contactFilter)
+    );
+  }
+);
